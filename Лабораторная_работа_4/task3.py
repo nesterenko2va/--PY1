e90ae0fb2080b4e0a515127a72febc2eb9b36825
nesterenko2va <115561502+nesterenko2va@users.noreply.github.com>
2022-11-02 18:43:27 +0300

    Лабораторная_работа_4

---
+++ Лабораторная_работа_4/task3.py
@@ -0,0 +1,14 @@
+def delete(list_, index=None):
+    if index==None :
+        index=-1
+        return list_[:index]
+    else:
+        return list_[:index] + list_[index+1:]
+
+    ...  # TODO реализовать функцию удаления элемента из списка по индексу
+
+
+
+print(delete([0, 0, 1, 2], index=0))# [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1)) # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
